97764535fa6ab24a66b36ea792bcc30a056da215
Pascal Stoppa <pascal.stoppa@me.thm.de>
2024-11-10 21:21:10 +0100

    alles als array geschrieben

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import numpy as np
 
 # Initialisiere Pygame
 pygame.init()
@@ -16,13 +17,15 @@ BODY2_COLOR = (255, 200, 100)
 
 # Body 1 settings
 body1_radius = 30
-body1_x, body1_y = WIDTH // 2 - 50, HEIGHT // 2 # integer division damit keine Pixel gesplittet werden
-body1_speed_x, body1_speed_y = 0, 0
+body1, body1 = np.array([WIDTH // 2 - 50, HEIGHT // 2]) # // integer division damit keine Pixel gesplittet werden
+body1_speed = np.array([0, 0])
+body1_acc = np.array([0, 0])
 
 # Body 2 settings
 body2_radius = 10
-body2_x, body2_y = WIDTH // 2 + 50, HEIGHT // 2
-body2_speed_x, body2_speed_y = 0, 0
+body2 = np.array([WIDTH // 2 + 50, HEIGHT // 2])
+body2_speed = np.array([0, 0])
+body2_acc = np.array([0, 0])
 
 # Animation loop
 clock = pygame.time.Clock()
@@ -35,17 +38,17 @@ while True:
             sys.exit()
             
     # Update body 1 position
-    body1_x += body1_speed_x
-    body1_y += body1_speed_y
+    body1_speed += body1_acc
+    body1 += body1_speed
     
     # Update body 2 position
-    body2_x += body2_speed_x
-    body2_y += body2_speed_y
+    body2_speed += body2_acc
+    body2 += body2_speed
     
     # Draw everything
     screen.fill(BACKGROUND_COLOR)
-    pygame.draw.circle(screen, BODY1_COLOR, (body1_x, body1_y), body1_radius)
-    pygame.draw.circle(screen, BODY2_COLOR, (body2_x, body2_y), body2_radius)
+    pygame.draw.circle(screen, BODY1_COLOR, body1, body1_radius)
+    pygame.draw.circle(screen, BODY2_COLOR, body2, body2_radius)
     
     # Update display
     pygame.display.flip()
