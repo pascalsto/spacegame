30709c6a92179e8a411a0876c6ee2f1cc8f8c8ea
Pascal Stoppa <pascal.stoppa@me.thm.de>
2024-11-10 21:03:20 +0100

    Setup erstellt mit 2 Körpern für 2 Körper Problem

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+import sys
+
+# Initialisiere Pygame
+pygame.init()
+
+# Screen settings
+WIDTH, HEIGHT = 1200, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Spacegame')
+
+# Colors
+BACKGROUND_COLOR = (0, 0, 0)
+BODY1_COLOR = (255, 100, 0)
+BODY2_COLOR = (255, 200, 100)
+
+# Body 1 settings
+body1_radius = 30
+body1_x, body1_y = WIDTH // 2 - 50, HEIGHT // 2 # integer division damit keine Pixel gesplittet werden
+body1_speed_x, body1_speed_y = 0, 0
+
+# Body 2 settings
+body2_radius = 10
+body2_x, body2_y = WIDTH // 2 + 50, HEIGHT // 2
+body2_speed_x, body2_speed_y = 0, 0
+
+# Animation loop
+clock = pygame.time.Clock()
+
+while True:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    # Update body 1 position
+    body1_x += body1_speed_x
+    body1_y += body1_speed_y
+    
+    # Update body 2 position
+    body2_x += body2_speed_x
+    body2_y += body2_speed_y
+    
+    # Draw everything
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.circle(screen, BODY1_COLOR, (body1_x, body1_y), body1_radius)
+    pygame.draw.circle(screen, BODY2_COLOR, (body2_x, body2_y), body2_radius)
+    
+    # Update display
+    pygame.display.flip()
+    
+    # Control the framerate
+    clock.tick(60)
